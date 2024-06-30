80a576e62a1ba03f024ab3bbf854a88f972f400e
pedro <pedro@localhost.localdomain>
2024-06-30 17:17:59 -0300

    view activity added completely

--- Appderestaurantekotlin/app/src/main/java/com/example/appderestaurante_kotlin/ViewRecipeActivity.kt
+++ Appderestaurantekotlin/app/src/main/java/com/example/appderestaurante_kotlin/ViewRecipeActivity.kt
@@ -1,10 +1,12 @@
+
 package com.example.appderestaurante_kotlin
 
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appderestaurante_kotlin.model.RecipeStore
 
-class ViewRecipeActivity : AppCompatActivity(){
+class ViewRecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.food_item_unit)
@@ -13,13 +15,15 @@ class ViewRecipeActivity : AppCompatActivity(){
         val txtReceiptIngredientsInfo: TextView = findViewById(R.id.txtReceiptIngredientsInfo)
         val txtReceiptIngredientsDescriptionInfo: TextView = findViewById(R.id.txtReceiptIngredientsDescriptionInfo)
 
-        val recipeName = intent.getStringExtra("RECIPE_NAME")
-        val recipeDescription = intent.getStringExtra("RECIPE_DESCRIPTION")
-        val recipePrice = intent.getStringExtra("RECIPE_PRICE")
+        val recipeId = intent.getIntExtra("RECIPE_ID", -1)
+        if (recipeId != -1) {
+            val recipeName = RecipeStore.receiptNames[recipeId]
+            val recipeIngredients = RecipeStore.receiptIngredients[recipeId]
+            val recipePreparation = RecipeStore.receiptPreparations[recipeId]
 
-
-        txtRecipeTitle.text = recipeName
-        txtReceiptIngredientsDescriptionInfo.text = recipeDescription
-        txtReceiptIngredientsInfo.text = recipePrice
+            txtRecipeTitle.text = recipeName
+            txtReceiptIngredientsInfo.text = recipeIngredients
+            txtReceiptIngredientsDescriptionInfo.text = recipePreparation
+        }
     }
 }
--- Appderestaurantekotlin/app/src/main/java/com/example/appderestaurante_kotlin/adapter/FoodAdapter.kt
+++ Appderestaurantekotlin/app/src/main/java/com/example/appderestaurante_kotlin/adapter/FoodAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.appderestaurante_kotlin.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.appderestaurante_kotlin.ViewRecipeActivity
 import com.example.appderestaurante_kotlin.databinding.FoodItemBinding
 import com.example.appderestaurante_kotlin.model.Food
 
@@ -23,6 +25,12 @@ class FoodAdapter (private val context: Context, private val foodList: List<Food
         holder.txtFoodName.text = food.foodName
         holder.txtFoodDescription.text = food.foodDescription
         holder.txtFoodId.text = "${food.id}"
+
+        holder.btnSeeReceipt.setOnClickListener {
+            val intent = Intent(context, ViewRecipeActivity::class.java)
+            intent.putExtra("RECIPE_ID", food.id)
+            context.startActivity(intent)
+        }
     }
 
     inner class FoodViewHolder(binding: FoodItemBinding): RecyclerView.ViewHolder(binding.root) {
@@ -30,6 +38,7 @@ class FoodAdapter (private val context: Context, private val foodList: List<Food
         val txtFoodName = binding.txtFoodName
         val txtFoodDescription = binding.txtFoodDescription
         val txtFoodId = binding.txtFoodId
+        val btnSeeReceipt = binding.btnSeeReceipt
 //        val txtPrice = binding.txtPrice
     }
 }
