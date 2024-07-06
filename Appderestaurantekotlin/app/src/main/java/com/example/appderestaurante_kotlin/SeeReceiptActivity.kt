c19c3c517a8d79d5dc4ab345c30326c4f8a953c2
pedro <pedro@localhost.localdomain>
2024-07-06 15:18:03 -0300

    bug reload see list refreshed done

--- Appderestaurantekotlin/app/src/main/java/com/example/appderestaurante_kotlin/SeeReceiptActivity.kt
+++ Appderestaurantekotlin/app/src/main/java/com/example/appderestaurante_kotlin/SeeReceiptActivity.kt
@@ -6,39 +6,52 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appderestaurante_kotlin.adapter.FoodAdapter
 import com.example.appderestaurante_kotlin.model.Food
-
-import com.example.appderestaurante_kotlin.R.id.RecyclerViewFood
 import com.example.appderestaurante_kotlin.model.RecipeStore
 
+class SeeReceiptActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FoodAdapter
+    private val foodList = mutableListOf<Food>()
 
-class SeeReceiptActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_receipt)
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewRecipes)
+
+        recyclerView = findViewById(R.id.recyclerViewRecipes)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
-        val foodList = getFoodList()
-        val adapter = FoodAdapter(this, foodList.toMutableList())
+        adapter = FoodAdapter(this, foodList)
         recyclerView.adapter = adapter
     }
 
-    private fun getFoodList(): List<Food>{
+    override fun onResume() {
+        super.onResume()
+        updateFoodList()
+    }
 
+    private fun updateFoodList() {
+        foodList.clear()
+        foodList.addAll(getFoodList())
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun getFoodList(): List<Food> {
         val foodList = mutableListOf<Food>()
-        for(i in RecipeStore.receiptNames.indices){
+        for (i in RecipeStore.receiptNames.indices) {
             val name = RecipeStore.receiptNames[i]
             val ingredients = RecipeStore.receiptIngredients[i]
             val preparation = RecipeStore.receiptPreparations[i]
             val foodDescription = preparation.take(50)
-            foodList.add(Food(
-                id = i,
-                imgFood = R.drawable.cooking,
-                foodName = name,
-                foodDescription = foodDescription,
-                price = ""
-            ))
+            foodList.add(
+                Food(
+                    id = i,
+                    imgFood = R.drawable.cooking,
+                    foodName = name,
+                    foodDescription = foodDescription,
+                    price = ""
+                )
+            )
         }
         return foodList
     }
